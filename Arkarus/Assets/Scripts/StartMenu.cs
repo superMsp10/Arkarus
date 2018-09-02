2c699ff1a5616468c9b29a6f9fdfaf21745c238e
superMsp10 <pandeymahan34@gmail.com>
2018-09-01 23:30:11 -0400

    Play Scene

---
+++ Arkarus/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public string PlaySceneName;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartPlayScene()
+    {
+        SceneManager.LoadScene(PlaySceneName);
+    }
+}
