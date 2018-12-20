94eb7dc498898f86dfa1167dae4d705172ccd425
superMsp10 <pandeymahan34@gmail.com>
2018-12-19 20:24:26 -0500

    Spawn position
    Ghosts only spawn outside camera frustrum but always in front of the camera in the local z direction

--- Arkarus/Assets/Scripts/GhostSpawner.cs
+++ Arkarus/Assets/Scripts/GhostSpawner.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class GhostSpawner : MonoBehaviour
 {
     public GameObject ghostPrefab;
-    public float spawnRange;
+    public float spawnRangeMin, spawnRangeMax, frustrumDestOffest = 0.5f;
     public int maxGhostCount, ghostSpawnRate;
     Pooler ghostPooler;
+    Camera fpCam ;
 
     // Start is called before the first frame update
     void Start()
     {
+        fpCam = GameManager.gameManager.FirstPersonCamera;
         ghostPooler = new Pooler(maxGhostCount, ghostPrefab);
         InvokeRepeating("SpawnGhosts", 0f, ghostSpawnRate);
     }
@@ -20,14 +22,27 @@ public class GhostSpawner : MonoBehaviour
     {
         if (ghostPooler.active.Count < maxGhostCount)
         {
-           GameObject g = ghostPooler.getObject();
-            g.transform.position = Random.onUnitSphere * spawnRange;
+            GameObject g = ghostPooler.getObject();
+            //Camera frustrum has 4 lines projecting out of the camera
+            // 1 --- 2
+            // |     |
+            // 3 --- 4
+            //Chose random line to start from
+            //Get direction using viewport point to world raycast
+            //Frustrum destination = direction * random depth
+            //Offset Direction: Difference between camera center position and frustrum destination normalized
+            // Final position = frustrum destination + offset direciton * randomMagnitude
+            float r_depth = Random.Range(spawnRangeMin, spawnRangeMax);
+            Vector2 r_corner = new Vector2(Random.Range(0, 2), Random.Range(0, 2));
+            Vector3 frustrumDest = fpCam.ViewportPointToRay(r_corner).direction * r_depth;
+            Vector3 offset = (frustrumDest - fpCam.transform.forward * r_depth).normalized * frustrumDestOffest;
+            g.transform.position = frustrumDest + offset;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
