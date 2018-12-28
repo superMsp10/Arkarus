078a3f04093adfb5cf1b928cae9d6f8915b831b9
superMsp10 <pandeymahan34@gmail.com>
2018-12-27 20:18:43 -0500

    Start Menu Text Color
    Animated text color

--- Arkarus/Assets/Scripts/StartMenu.cs
+++ Arkarus/Assets/Scripts/StartMenu.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class StartMenu : MonoBehaviour
 {
     public string PlaySceneName;
+    public TextMeshProUGUI text;
+    public Color color1, color2;
+
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        text.faceColor = Color.Lerp(color1, color2, Mathf.Abs(Mathf.Sin(Time.time/2)));
+        text.outlineColor = Color.Lerp(color2, color1, Mathf.Abs(Mathf.Sin(Time.time/2)));
 
     }
 
